e9c75257f7d50285216d9b5fdbd2519b6fadebb9
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-13 21:55:20 +0100

    refactor(exception): Add 'errors' field to ErrorResponse DTO

--- src/main/java/com/progressoft/progresssoft_fx_importer/exception/ErrorResponse.java
+++ src/main/java/com/progressoft/progresssoft_fx_importer/exception/ErrorResponse.java
@@ -3,9 +3,11 @@ package com.progressoft.progresssoft_fx_importer.exception;
 import java.time.LocalDateTime;
 
 public record ErrorResponse(
+        int statusCode,
         LocalDateTime timestamp,
         String message,
-        String description
+        String description,
+        Object errors
 
 ) {
 }
