22c66ea5e9967c3b3620bf4669d865799c022cb9
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-13 21:13:02 +0100

    feat: Create DealRequestDto as record with validation

---
+++ src/main/java/com/oumaima/assignment/progresssoft_fx_importer/dto/DealRequestDto.java
@@ -0,0 +1,27 @@
+package com.oumaima.assignment.progresssoft_fx_importer.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+public record DealRequestDto (
+
+    @NotBlank(message = "Deal Unique Id cannot be blank")
+    String dealUniqueId,
+    @NotNull(message = "From Currency cannot be null")
+    Currency fromCurrency,
+    @NotNull(message = "To Currency cannot be null")
+    Currency toCurrency,
+    @NotNull(message = "Deal timestamp cannot be null")
+    LocalDateTime dealTimestamp,
+    @NotNull(message = "Deal amount cannot be null")
+    @Positive(message = "Deal amount must be positive")
+    BigDecimal dealAmount
+
+){
+
+}
