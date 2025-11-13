d7e70a313bb6bb6c235a43a81519b0a627340cd4
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-13 21:14:39 +0100

    feat: Create DealResponseDto as record

---
+++ src/main/java/com/oumaima/assignment/progresssoft_fx_importer/dto/DealResponseDto.java
@@ -0,0 +1,15 @@
+package com.oumaima.assignment.progresssoft_fx_importer.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+public record DealResponseDto (
+
+        String dealUniqueId,
+        Currency fromCurrency,
+        Currency toCurrency,
+        LocalDateTime dealTimestamp,
+        BigDecimal dealAmount
+){
+}
