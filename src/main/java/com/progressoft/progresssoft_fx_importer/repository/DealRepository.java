e53d8f36c40c8f4e1bb13dcfb1be9bfafc584d7d
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-11-13 22:28:24 +0100

    feat(repository): Create DealRepository interface

--- src/main/java/com/progressoft/progresssoft_fx_importer/repository/DealRepository.java
+++ src/main/java/com/progressoft/progresssoft_fx_importer/repository/DealRepository.java
@@ -1,4 +1,8 @@
 package com.progressoft.progresssoft_fx_importer.repository;
 
-public class DealRepository {
+import com.progressoft.progresssoft_fx_importer.model.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DealRepository extends JpaRepository<Deal, String> {
+
 }
